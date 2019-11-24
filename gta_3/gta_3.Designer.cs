983a66b16e32b42e5e99a6a0245fbd00de7977b6
bogachenko <bogachenkove@gmail.com>
2019-11-24 19:33:24 +1100

    Created gta_3

---
+++ gta_3/gta_3.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace gta_3
+{
+    partial class gta_3
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // gta_3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 111);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "gta_3";
+            this.ShowIcon = false;
+            this.Text = "Launcher for Grand Theft Auto III";
+            this.Load += new System.EventHandler(this.gta_3_load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ gta_3/gta_3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace gta_3
+{
+    public partial class gta_3 : Form
+    {
+        public gta_3() => InitializeComponent();
+
+        private void gta_3_load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
