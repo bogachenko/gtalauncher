b91090b386115110107239684938ddd2447bd145
bogachenko <bogachenkove@gmail.com>
2019-11-28 18:10:03 +1100

    Create files

---
+++ gta_vc/gta_vc/gta_vc.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace gta_vc
+{
+    partial class gta_vc
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // gta_vc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 161);
+            this.Name = "gta_vc";
+            this.Text = "Launcher for Grand Theft Auto Vice City";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
