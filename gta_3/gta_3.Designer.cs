8681a58295c235406b4ce0efcef3e3567df6facc
bogachenko <bogachenkove@gmail.com>
2019-11-28 20:37:29 +1100

    Update gta_3

--- gta_3/gta_3.Designer.cs
+++ gta_3/gta_3.Designer.cs
@@ -8,30 +8,40 @@ namespace gta_3 {
    base.Dispose(disposing);
   }
   private void InitializeComponent() {
-   System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(gta_3));
-   this.lu = new System.Windows.Forms.Button();
-   this.gta_iii = new System.Windows.Forms.Button();
-   this.SuspendLayout();
-   resources.ApplyResources(this.lu, "lu");
-   this.lu.Name = "lu";
-   this.lu.UseVisualStyleBackColor = true;
-   this.lu.Click += new System.EventHandler(this.click_lu);
-   resources.ApplyResources(this.gta_iii, "gta_iii");
-   this.gta_iii.Name = "gta_iii";
-   this.gta_iii.UseVisualStyleBackColor = true;
-   this.gta_iii.Click += new System.EventHandler(this.click_gta_3);
-   resources.ApplyResources(this, "$this");
-   this.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
-   this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-   this.Controls.Add(this.gta_iii);
-   this.Controls.Add(this.lu);
-   this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-   this.MaximizeBox = false;
-   this.MinimizeBox = false;
-   this.Name = "gta_3";
-   this.ShowIcon = false;
-   this.Load += new System.EventHandler(this.gta_3_load);
-   this.ResumeLayout(false);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(gta_3));
+            this.lu = new System.Windows.Forms.Button();
+            this.gta_iii = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lu
+            // 
+            resources.ApplyResources(this.lu, "lu");
+            this.lu.Name = "lu";
+            this.lu.UseVisualStyleBackColor = true;
+            this.lu.Click += new System.EventHandler(this.click_lu);
+            // 
+            // gta_iii
+            // 
+            resources.ApplyResources(this.gta_iii, "gta_iii");
+            this.gta_iii.Name = "gta_iii";
+            this.gta_iii.UseVisualStyleBackColor = true;
+            this.gta_iii.Click += new System.EventHandler(this.click_gta_3);
+            // 
+            // gta_3
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.gta_iii);
+            this.Controls.Add(this.lu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "gta_3";
+            this.ShowIcon = false;
+            this.Load += new System.EventHandler(this.gta_3_load);
+            this.ResumeLayout(false);
+
   }
   private System.Windows.Forms.Button lu;
   private System.Windows.Forms.Button gta_iii;
