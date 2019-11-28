d3a7d8d805bf4b2692547c15d254ce4fdb38de6a
bogachenko <bogachenkove@gmail.com>
2019-11-28 21:18:58 +1100

    Update gta_sa.cs

--- gta_sa/gta_sa.cs
+++ gta_sa/gta_sa.cs
@@ -11,9 +11,33 @@ using System.Windows.Forms;
 namespace gta_sa {
  public partial class gta_sa: Form {
   public gta_sa() {
+   TopMost = true;
+   this.StartPosition = FormStartPosition.CenterScreen;
    InitializeComponent();
   }
-
+  private void Gta_sa_click(object sender, EventArgs e) {
+   {
+    if (File.Exists("gta_sa.exe")) {
+     Process.Start("gta_sa.exe");
+     Close();
+    } else {
+     MessageBox.Show("I can not find the gta_sa.exe file," + "check its availability and try again.", "File not found", MessageBoxButtons.OK);
+     Close();
+    }
+   }
+  }
+  private void Samp_click(object sender, EventArgs e) {
+   if (File.Exists("samp.exe")) {
+    Process.Start("samp.exe");
+    Close();
+   } else {
+    DialogResult result = MessageBox.Show("It looks like you do not have SAMP installed," + "click \"Yes\" to go to the developer's website.", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+    if (result == DialogResult.Yes) {
+     Process.Start("https://www.sa-mp.com/");
+    } else if (result == DialogResult.No)
+     Close(); {}
+   }
+  }
   private void gta_sa_load(object sender, EventArgs e) {
 
   }
