449ed24f5d9fb6dc2f8cf5c1c3f48f038288e28c
bogachenko <bogachenkove@gmail.com>
2019-11-28 18:17:48 +1100

    Update files

--- gta_vc/gta_vc.Designer.cs
+++ gta_vc/gta_vc.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace gta_vc
+namespace gta_vc
 {
     partial class gta_vc
     {
@@ -28,20 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.gtavc = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // gtavc
+            // 
+            this.gtavc.Location = new System.Drawing.Point(9, 18);
+            this.gtavc.Name = "gtavc";
+            this.gtavc.Size = new System.Drawing.Size(75, 23);
+            this.gtavc.TabIndex = 0;
+            this.gtavc.Text = "gta vc";
+            this.gtavc.UseVisualStyleBackColor = true;
+            this.gtavc.Click += new System.EventHandler(this.gta_vc_Click);
             // 
             // gta_vc
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 161);
+            this.Controls.Add(this.gtavc);
             this.Name = "gta_vc";
             this.Text = "Launcher for Grand Theft Auto Vice City";
+            this.Load += new System.EventHandler(this.gta_vc_load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button gtavc;
     }
 }
 
---
+++ gta_vc/gta_vc.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+namespace gta_vc
+{
+    public partial class gta_vc : Form
+    {
+        public gta_vc()
+        {
+            InitializeComponent();
+        }
+
+        private void gta_vc_Click(object sender, EventArgs e)
+        {
+            {
+                if (File.Exists("gta_vc.exe"))
+                {
+                    Process.Start("gta_vc.exe");
+                    Close();
+                }
+                else
+                {
+                    MessageBox.Show("I can not find the gta_vc.exe file," +
+                     "check its availability and try again.", "File not found", MessageBoxButtons.OK);
+                    Close();
+                }
+            }
+        }
+        private void vcmp_Click(object sender, EventArgs e)
+        {
+            if (File.Exists("VCMPBrowser.exe"))
+            {
+                Process.Start("VCMPBrowser.exe");
+                Close();
+            }
+            else
+            {
+                DialogResult result = MessageBox.Show("It looks like you do not have VC-MP installed," +
+                 "click \"Yes\" to go to the developer's website.", "Warning",
+                 MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (result == DialogResult.Yes)
+                {
+                    Process.Start("https://www.vc-mp.org/");
+                }
+                else if (result == DialogResult.No)
+                    Close();
+                { }
+            }
+        }
+        private void gta_vc_load(object sender, EventArgs e) { }
+    }
+}
