f53297fb4ffd611368a7bfe6d76ed7abfebc9e56
bogachenko <bogachenkove@gmail.com>
2022-12-06 05:58:37 +1100

    Update gta_sa

---
+++ gta_sa/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Launcher for Grand Theft Auto: San Andreas")]
+[assembly: AssemblyDescription("Simply launcher for Grand Theft Auto: San Andreas")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("gta-sa")]
+[assembly: AssemblyCopyright("Copyright © Bogachenko Vyacheslav")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("dcb76c43-59c5-4892-a03d-89ea4f5c326d")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.6")]
+[assembly: AssemblyFileVersion("1.0.0.6")]
+[assembly: NeutralResourcesLanguage("en-US")]
---
+++ gta_sa/gta-sa.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace gta_sa
+{
+    partial class gta_sa
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(gta_sa));
+            this.gtasa = new System.Windows.Forms.Button();
+            this.gtasade = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // gtasa
+            // 
+            this.gtasa.Location = new System.Drawing.Point(12, 13);
+            this.gtasa.Name = "gtasa";
+            this.gtasa.Size = new System.Drawing.Size(460, 43);
+            this.gtasa.TabIndex = 0;
+            this.gtasa.Text = "Grand Theft Auto: San Andreas";
+            this.gtasa.UseVisualStyleBackColor = true;
+            this.gtasa.Click += new System.EventHandler(this.gtasa_click);
+            // 
+            // gtasade
+            // 
+            this.gtasade.Location = new System.Drawing.Point(12, 62);
+            this.gtasade.Name = "gtasade";
+            this.gtasade.Size = new System.Drawing.Size(460, 43);
+            this.gtasade.TabIndex = 1;
+            this.gtasade.Text = "Grand Theft Auto: San Andreas – The Definitive Edition";
+            this.gtasade.UseVisualStyleBackColor = true;
+            this.gtasade.Click += new System.EventHandler(this.gtasade_click);
+            // 
+            // gta_sa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 120);
+            this.Controls.Add(this.gtasade);
+            this.Controls.Add(this.gtasa);
+            this.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "gta_sa";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Launcher for the Grand Theft Auto: San Andreas";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button gtasa;
+        private System.Windows.Forms.Button gtasade;
+    }
+}
+
