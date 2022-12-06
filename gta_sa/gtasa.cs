a3efe18cb8e33e02789c581ee9de19c7d37c0f24
bogachenko <bogachenkove@gmail.com>
2022-12-06 19:19:02 +1100

    Update gta_sa

--- gta_sa/gtasa.cs
+++ gta_sa/gtasa.cs
@@ -1,15 +1,15 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-namespace gta_sa {
-  internal static class Program {
-    [STAThread]
-    static void Main() {
-      Application.EnableVisualStyles();
-      Application.SetCompatibleTextRenderingDefault(false);
-      Application.Run(new gta_sa());
+namespace gta_sa
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new gta_sa());
+        }
     }
-  }
 }
