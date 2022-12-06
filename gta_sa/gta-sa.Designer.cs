f236ee1f8211376cfa44eeba69287a9323944f1f
bogachenko <bogachenkove@gmail.com>
2022-12-06 18:30:20 +1100

    Update gta-sa.Designer.cs

--- gta_sa/gta-sa.Designer.cs
+++ gta_sa/gta-sa.Designer.cs
@@ -2,15 +2,7 @@
 {
     partial class gta_sa
     {
-        /// <summary>
-        /// Обязательная переменная конструктора.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
-
-        /// <summary>
-        /// Освободить все используемые ресурсы.
-        /// </summary>
-        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -19,22 +11,12 @@
             }
             base.Dispose(disposing);
         }
-
-        #region Код, автоматически созданный конструктором форм Windows
-
-        /// <summary>
-        /// Требуемый метод для поддержки конструктора — не изменяйте 
-        /// содержимое этого метода с помощью редактора кода.
-        /// </summary>
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(gta_sa));
             this.gtasa = new System.Windows.Forms.Button();
             this.gtasade = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // gtasa
-            // 
             this.gtasa.Location = new System.Drawing.Point(12, 13);
             this.gtasa.Name = "gtasa";
             this.gtasa.Size = new System.Drawing.Size(460, 43);
@@ -42,9 +24,6 @@
             this.gtasa.Text = "Grand Theft Auto: San Andreas";
             this.gtasa.UseVisualStyleBackColor = true;
             this.gtasa.Click += new System.EventHandler(this.gtasa_click);
-            // 
-            // gtasade
-            // 
             this.gtasade.Location = new System.Drawing.Point(12, 62);
             this.gtasade.Name = "gtasade";
             this.gtasade.Size = new System.Drawing.Size(460, 43);
@@ -52,9 +31,6 @@
             this.gtasade.Text = "Grand Theft Auto: San Andreas – The Definitive Edition";
             this.gtasade.UseVisualStyleBackColor = true;
             this.gtasade.Click += new System.EventHandler(this.gtasade_click);
-            // 
-            // gta_sa
-            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 120);
@@ -70,13 +46,9 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Launcher for the Grand Theft Auto: San Andreas";
             this.ResumeLayout(false);
-
         }
-
         #endregion
-
         private System.Windows.Forms.Button gtasa;
         private System.Windows.Forms.Button gtasade;
     }
 }
-
