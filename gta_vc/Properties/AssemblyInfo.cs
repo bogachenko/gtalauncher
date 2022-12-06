75b613b64c8f4e6c75670a3a5d6161255f2093c7
bogachenko <bogachenkove@gmail.com>
2022-12-06 23:51:59 +1100

    Update gta_vc

--- gta_vc/Properties/AssemblyInfo.cs
+++ gta_vc/Properties/AssemblyInfo.cs
@@ -1,11 +1,7 @@
-﻿using System.Resources;
+using System.Resources;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// Общие сведения об этой сборке предоставляются следующим набором
-// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
-// связанных со сборкой.
 [assembly: AssemblyTitle("Launcher for Grand Theft Auto: Vice City")]
 [assembly: AssemblyDescription("Simply launcher for Grand Theft Auto: Vice City")]
 [assembly: AssemblyConfiguration("")]
@@ -14,25 +10,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Bogachenko Vyacheslav")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
-// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
-// COM, следует установить атрибут ComVisible в TRUE для этого типа.
 [assembly: ComVisible(false)]
-
-// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
 [assembly: Guid("381b68d4-6b8c-4bb2-9bea-2b4c87a0b590")]
-
-// Сведения о версии сборки состоят из указанных ниже четырех значений:
-//
-//      Основной номер версии
-//      Дополнительный номер версии
-//      Номер сборки
-//      Редакция
-//
-// Можно задать все значения или принять номера сборки и редакции по умолчанию 
-// используя "*", как показано ниже:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.2")]
-[assembly: AssemblyFileVersion("1.0.1.2")]
+[assembly: AssemblyVersion("1.0.1.3")]
+[assembly: AssemblyFileVersion("1.0.1.3")]
 [assembly: NeutralResourcesLanguage("en-US")]
