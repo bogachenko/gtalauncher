4155b22a8aea07a008e99d1502a679359ae50b35
bogachenko <bogachenkove@gmail.com>
2022-12-07 00:28:39 +1100

    Update files

--- gta_vc/Properties/Settings.Designer.cs
+++ gta_vc/Properties/Settings.Designer.cs
@@ -1,5 +1,5 @@
 namespace gta_vc.Properties {
-[global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
     internal sealed partial class Settings: global::System.Configuration.ApplicationSettingsBase {
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
