5b076ba64676ef23296e7f9ce63148772293cf3d
bogachenko <bogachenkove@gmail.com>
2022-12-06 23:28:20 +1100

    Update gta_3

--- gta_3/gta_3.Designer.cs
+++ gta_3/gta_3.Designer.cs
@@ -13,23 +13,14 @@ namespace gta_3 {
             this.gta3 = new System.Windows.Forms.Button();
             this.gta3de = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // gta3
-            // 
             resources.ApplyResources(this.gta3, "gta3");
             this.gta3.Name = "gta3";
             this.gta3.UseVisualStyleBackColor = true;
             this.gta3.Click += new System.EventHandler(this.gta3_click);
-            // 
-            // gta3de
-            // 
             resources.ApplyResources(this.gta3de, "gta3de");
             this.gta3de.Name = "gta3de";
             this.gta3de.UseVisualStyleBackColor = true;
             this.gta3de.Click += new System.EventHandler(this.gta3de_click);
-            // 
-            // gta_3
-            // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.gta3de);
